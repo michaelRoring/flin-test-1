64bebd329aac9648358bd01e890c3d252409a8fb
michael <roringmichael@gmail.com>
2025-05-15 15:08:13 +0700

    feat: add allowed list

--- chatbot-backend/src/index.ts
+++ chatbot-backend/src/index.ts
@@ -5,14 +5,32 @@ import chatRoutes from "./routes/chatRoutes";
 
 const app = express();
 
-// Middleware
+const allowedOrigins = [
+  "localhost:3000",
+  "localhost:3001",
+  "https://flin-test-1.pages.dev/",
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  credentials: true,
+};
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/", chatRoutes);
 
-// Start server
 app.listen(config.port, () => {
   console.log(`Backend server listening on http://localhost:${config.port}`);
 });
--- chatbot-frontend/src/components/Chatbot.tsx
+++ chatbot-frontend/src/components/Chatbot.tsx
@@ -11,7 +11,8 @@ interface ChatbotProps {
   toggle: () => void;
 }
 
-const apiUrl = import.meta.env.API_URL || "http://localhost:3001";
+console.log(" import.meta.env.VITE_API_URL :", import.meta.env.VITE_API_URL);
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export default function Chatbot({ toggle }: ChatbotProps) {
   const [messages, setMessages] = useState<Message[]>([]);
